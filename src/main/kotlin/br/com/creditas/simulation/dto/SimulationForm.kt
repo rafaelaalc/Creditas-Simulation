c052f10dc8a7ce09a32607c56cec8b7a96db74d0
Rafaela Alcantara <rafaelaalc_@outlook.com>
2025-08-04 22:07:14 -0300

    :art: realiza ajustes na service

--- src/main/kotlin/br/com/creditas/simulation/dto/SimulationForm.kt
+++ src/main/kotlin/br/com/creditas/simulation/dto/SimulationForm.kt
@@ -1,11 +1,9 @@
 package br.com.creditas.simulation.dto
 
-import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class SimulationForm(
     val value: Double,
-    @JsonFormat(pattern = "dd/MM/yyyy")
     val dateOfBirth: LocalDate,
     val paymentTerm: Int,
 )
--- src/main/kotlin/br/com/creditas/simulation/service/CreditSimulationService.kt
+++ src/main/kotlin/br/com/creditas/simulation/service/CreditSimulationService.kt
@@ -3,9 +3,12 @@ package br.com.creditas.simulation.service
 import br.com.creditas.simulation.dto.SimulationForm
 import br.com.creditas.simulation.dto.SimulationView
 import br.com.creditas.simulation.exception.BadRequestException
+import br.com.creditas.simulation.exception.InternalServerErrorException
 import br.com.creditas.simulation.helper.formatAmount
 import br.com.creditas.simulation.helper.validateForm
+import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 import java.time.LocalDate
 import kotlin.math.pow
 
@@ -21,19 +24,26 @@ class CreditSimulationService {
     fun simulate(
         simulation: SimulationForm,
     ): SimulationView {
-        validateForm(simulation)
-        val annualInterestRate = getAnnualInterestRate(simulation.dateOfBirth)
-        val monthlyInterestRate = (annualInterestRate / MONTHS_VALUE) / HUNDRED_VALUE
-        val monthlyPayment = (simulation.value * monthlyInterestRate) /
-            (1 - (1 + monthlyInterestRate).pow(-simulation.paymentTerm))
+        try {
+            validateForm(simulation)
+            val annualInterestRate = getAnnualInterestRate(simulation.dateOfBirth)
+            val monthlyInterestRate = (annualInterestRate / MONTHS_VALUE) / HUNDRED_VALUE
+            val monthlyPayment = (simulation.value * monthlyInterestRate) /
+                (1 - (1 + monthlyInterestRate).pow(-simulation.paymentTerm))
 
-        val total = (monthlyPayment * simulation.paymentTerm)
-        val totalInterest = (monthlyPayment * simulation.paymentTerm) - simulation.value
-        return SimulationView(
-            total = formatAmount(total),
-            installmentsValue = formatAmount(monthlyPayment),
-            totalInterest = formatAmount(totalInterest),
-        )
+            val total = (monthlyPayment * simulation.paymentTerm)
+            val totalInterest = (monthlyPayment * simulation.paymentTerm) - simulation.value
+            return SimulationView(
+                total = formatAmount(total),
+                installmentsValue = formatAmount(monthlyPayment),
+                totalInterest = formatAmount(totalInterest),
+            )
+        } catch (e: HttpClientErrorException) {
+            when (HttpStatusCode.valueOf(e.statusCode.value()).is4xxClientError) {
+                true -> throw BadRequestException("")
+                else -> throw InternalServerErrorException("Um erro inesperado aconteceu, por favor, tente novamente.")
+            }
+        }
     }
 
     private fun getAnnualInterestRate(dateOfBirth: LocalDate): Double {
